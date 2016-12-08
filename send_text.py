44a176aa43694112fc5990220b5d15b67dc29337
shashank7200 <shashank7200@gmail.com>
2016-12-08 17:42:05 +0530

    Send Text Messages
    Send Text Messages

---
+++ send_text.py
@@ -0,0 +1,12 @@
+from twilio.rest import TwilioRestClient
+
+account_sid = "AC503d380f08fe389a6b96b01bd1c72a0d" # Your Account SID from www.twilio.com/console
+auth_token  = "7b1ac4f409bbbb9f0f7623c813583814"  # Your Auth Token from www.twilio.com/console
+
+client = TwilioRestClient(account_sid, auth_token)
+
+message = client.messages.create(body="Hello from Python",
+    to="+918587099540",    # Replace with your phone number
+    from_="+14086229325") # Replace with your Twilio number
+
+print(message.sid)
