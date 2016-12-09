5a3ad7f3b91027a537dc385c089e8bf1df023f26
shashank7200 <shashank7200@gmail.com>
2016-12-09 15:43:30 +0530

    Sents Text Messages
    Sents Text messages using Twilio Api

--- send_text.py
+++ send_text.py
@@ -1,12 +1,12 @@
 from twilio import rest
 
-account_sid = "AC503d380f08fe389a6b96b01bd1c72a0d" # Your Account SID from www.twilio.com/console
-auth_token  = "7b1ac4f409bbbb9f0f7623c813583814"  # Your Auth Token from www.twilio.com/console
+account_sid = "" # Your Account SID from www.twilio.com/console
+auth_token  = ""  # Your Auth Token from www.twilio.com/console
 
 client = rest.TwilioRestClient(account_sid, auth_token)
 
 message = client.messages.create(body="This is me, Shashank !",
-    to="+918587099540",    # Replace with your phone number
-    from_="+14086229325") # Replace with your Twilio number
+    to="",    # Replace with your phone number
+    from_="") # Replace with your Twilio number
 
 print(message.sid)
