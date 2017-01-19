86a5050c5ee9fd11fde9468c6d8d3779046964ae
shashank7200 <shashank7200@gmail.com>
2017-01-19 16:19:31 +0530

    Fixed URL Redirection

--- break-time.py
+++ break-time.py
@@ -7,5 +7,5 @@ break_count=0
 print("This program was started on"+time.ctime())
 while break_count<total_breaks:
      time.sleep(10)
-     webbrowser.open("https://www.youtube.com/watch?v=ZFQM5yV0aHchttps://www.youtube.com/watch?v=ZFQM5yV0aHc")
+     webbrowser.open("https://www.youtube.com/watch?v=ZFQM5yV0aHc")
      break_count=break_count+1
